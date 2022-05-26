9e816a9bec8e91c77967939913bccad4ca123c75
punit-naik <naik.punit44@gmail.com>
2022-05-26 18:06:53 +0530

    allowed passing allowed warning regexes

--- src/org/clojars/punit_naik/html_parser/core.clj
+++ src/org/clojars/punit_naik/html_parser/core.clj
@@ -8,20 +8,22 @@
 
 (defn warning-allowed?
   "Checks if the warning string as allowed warning which should not be deemed as an error"
-  [warning]
+  [warning & allowed-warning-regexes]
   (or (str/starts-with? warning "plain text isn't allowed in")
       (str/starts-with? warning "unknown attribute")
       (= warning "inserting missing 'title' element")
-      (= warning "missing <!DOCTYPE> declaration")))
+      (= warning "missing <!DOCTYPE> declaration")
+      (some #(not (nil? (re-matches % warning)))
+            allowed-warning-regexes)))
 
 (defn parse-warning-messages
-  [error-output]
+  [error-output & allowed-warning-regexes]
   (keep
    (fn [error-text]
      (when-let [warning-message (and (seq (re-matches #"line\s[0-9]+\scolumn\s[0-9]+.*"
                                                       error-text))
                                      (second (str/split error-text #"\:\s")))]
-       {:allowed? (warning-allowed? warning-message)
+       {:allowed? (apply warning-allowed? warning-message allowed-warning-regexes)
         :message warning-message}))
    (str/split error-output #"\n")))
 
@@ -43,14 +45,14 @@
   "Parses an html string and returns the parsed body
    Also returns all warning messages and whether they are allowed or not
    And also the error count which is parser errors + disallowed warnings count"
-  [tidy-obj html-string]
+  [tidy-obj html-string & allowed-warning-regexes]
   (with-open [in-stream (ByteArrayInputStream. (.getBytes html-string))
               out-stream (ByteArrayOutputStream.)
               err-stream (ByteArrayOutputStream.)
               err-writer (create-error-writer err-stream)]
     (.setErrout tidy-obj err-writer)
     (.parse tidy-obj in-stream out-stream)
-    (let [warnings (parse-warning-messages (str err-stream))]
+    (let [warnings (apply parse-warning-messages (str err-stream) allowed-warning-regexes)]
       {:parsed-output (-> (str out-stream)
                           str/trim-newline
                           str/trim)
--- test/org/clojars/punit_naik/html_parser/core_test.clj
+++ test/org/clojars/punit_naik/html_parser/core_test.clj
@@ -8,6 +8,7 @@
     (is (hp/warning-allowed? "unknown attribute \"test\""))
     (is (hp/warning-allowed? "inserting missing 'title' element"))
     (is (hp/warning-allowed? "missing <!DOCTYPE> declaration"))
+    (is (hp/warning-allowed? "missing </a>" #"missing\s\<\/[a-z]+\>"))
     (is (not (hp/warning-allowed? "missing </div>")))
     (is (not (hp/warning-allowed? "missing </a>")))))
 
@@ -20,7 +21,17 @@
                  "InputStream: Document content looks like HTML 4.01\n"
                  "3 warnings, no errors were found!\n"))
            [{:message "unknown attribute \"asdsdas\"", :allowed? true}
-            {:message "missing </div>", :allowed? false}
+            {:message "missing </div>", :allowed? nil}
+            {:message "inserting missing 'title' element", :allowed? true}]))
+    (is (= (hp/parse-warning-messages
+            (str "line 1 column 1 - Warning: unknown attribute \"asdsdas\"\n"
+                 "line 1 column 31 - Warning: missing </div>\n"
+                 "line 1 column 32 - Warning: inserting missing 'title' element\n"
+                 "InputStream: Document content looks like HTML 4.01\n"
+                 "3 warnings, no errors were found!\n")
+            #"missing\s\<\/div\>")
+           [{:message "unknown attribute \"asdsdas\"", :allowed? true}
+            {:message "missing </div>", :allowed? true}
             {:message "inserting missing 'title' element", :allowed? true}]))))
 
 (deftest parse-html-test
@@ -34,15 +45,21 @@
            {:parsed-output "<div>punit naik</div>",
             :error-count 1,
             :warnings
-            [{:message "missing </div>", :allowed? false}
+            [{:message "missing </div>", :allowed? nil}
              {:message "inserting missing 'title' element", :allowed? true}]}))
     (is (= (hp/parse-html (hp/create-tidy-object) "<dev>punit naik</div>")
            {:parsed-output "",
             :error-count 4,
             :warnings
-            [{:message "<dev> is not recognized!", :allowed? false}
-             {:message "discarding unexpected <dev>", :allowed? false}
+            [{:message "<dev> is not recognized!", :allowed? nil}
+             {:message "discarding unexpected <dev>", :allowed? nil}
              {:message "plain text isn't allowed in <head> elements",
               :allowed? true}
-             {:message "discarding unexpected </div>", :allowed? false}
+             {:message "discarding unexpected </div>", :allowed? nil}
+             {:message "inserting missing 'title' element", :allowed? true}]}))
+    (is (= (hp/parse-html (hp/create-tidy-object) "<div>punit naik" #"missing\s\<\/div\>")
+           {:parsed-output "<div>punit naik</div>",
+            :error-count 0,
+            :warnings
+            [{:message "missing </div>", :allowed? true}
              {:message "inserting missing 'title' element", :allowed? true}]}))))
