1020e92a7ddebd611690bc9123fa2fdea6d6e9e8
punit-naik <naik.punit44@gmail.com>
2022-05-26 18:53:06 +0530

    bumped version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.punit-naik/html-parser "1.0.1"
+(defproject org.clojars.punit-naik/html-parser "1.0.2"
   :description "A Clojure library designed to parse HTML string and return any errors and warnings while parsing"
   :url "https://github.com/punit-naik/html-parser"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
